c5152819b971cd623a1aa8f99d8df55cefc45870
Laura <almasilaura@outlook.com>
2020-11-26 11:50:30 +0000

    3rd test PASS

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,5 +1,7 @@
 def fizzbuzz(number): # we don't do a commit until our first test pass
     if number == 3:
         return "Fizz"
-    else:
+    elif number == 5:
         return "Buzz"
+    else:
+        return "FizzBuzz"
--- tests/fizzbuzz_test.py
+++ tests/fizzbuzz_test.py
@@ -6,5 +6,10 @@ class TestFizzBuzz(unittest.TestCase):
     def test_fizzbuzz_3_returns_fizz(self):
         self.assertEqual("Fizz", fizzbuzz(3))
 
-    def test_fizzbuzz_5_returns_buzz(self):
+    def test_fizzbuzz_15_returns_buzz(self):
 	    self.assertEqual("Buzz", fizzbuzz(5))
+
+    def test_fizzbuzz_5_returns_fizzbuzz(self):
+	    self.assertEqual("FizzBuzz", fizzbuzz(15))
+
+  
